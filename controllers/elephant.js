7a9670bf0d3b108a382a8b6bf00c3c861504b62e
anudeepsagar <68767594+anudeepsagar@users.noreply.github.com>
2022-11-09 22:14:46 -0600

    updated

--- controllers/elephant.js
+++ controllers/elephant.js
@@ -1,30 +1,64 @@
 var Elephant = require('../models/elephant');
 // List of all Elephants
-exports.elephant_list = function(req, res) {
- res.send('NOT IMPLEMENTED: Elephant list');
+exports.elephant_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: Elephant list');
 };
 // for a specific Elephant.
-exports.elephant_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: Elephant detail: ' + req.params.id);
+exports.elephant_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Elephant detail: ' + req.params.id);
 };
 // Handle Elephant create on POST.
-exports.elephant_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Elephant create POST');
+exports.elephant_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: Elephant create POST');
 };
 // Handle Elephant delete form on DELETE.
-exports.elephant_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: Elephant delete DELETE ' + req.params.id);
+exports.elephant_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Elephant delete DELETE ' + req.params.id);
 };
 // Handle Elephant update form on PUT.
-exports.elephant_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: Elephant update PUT' + req.params.id);
+exports.elephant_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Elephant update PUT' + req.params.id);
 };
 
 // List of all Elephants
-exports.elephant_list = async function(req, res) {
+exports.elephant_list = async function (req, res) {
+    try {
+        theElephants = await Elephant.find();
+        res.send(theElephants);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// VIEWS
+// Handle a show all view
+exports.elephant_view_all_Page = async function(req, res) {
     try{
     theElephants = await Elephant.find();
-    res.send(theElephants);
+    res.render('elephant', { title: 'Elephant Search Results', results: theElephants });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+
+   // Handle Costume create on POST.
+exports.elephant_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Elephant();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.elephant_type = req.body.elephant_type;
+    document.elephant_age_limit = req.body.elephant_age_limit;
+    document.elephant_weight = req.body.elephant_weight;
+    try{
+    let result = await document.save();
+    res.send(result);
     }
     catch(err){
     res.status(500);
--- routes/elephant.js
+++ routes/elephant.js
@@ -1,9 +1,15 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('elephant', { title: 'Search Results for elephants' });
+// });
+
+// module.exports = router;
 var express = require('express');
+const elephant_controlers= require('../controllers/elephant');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('elephant', { title: 'Search Results for elephants' });
-});
-
+/* GET costumes */
+router.get('/', elephant_controlers.elephant_view_all_Page );
 module.exports = router;
