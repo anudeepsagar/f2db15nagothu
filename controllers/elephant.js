4761df5a2ea8d8b1bdb3d9d0fd4dfab6d6d11bfc
anudeepsagar <68767594+anudeepsagar@users.noreply.github.com>
2022-11-16 18:23:39 -0600

    updated

--- controllers/elephant.js
+++ controllers/elephant.js
@@ -93,3 +93,58 @@ ${JSON.stringify(req.body)}`)
 failed`);
 }
 };
+
+// Handle Costume delete on DELETE. 
+exports.elephant_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Elephant.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+
+ 
+ // Handle a show one view with id specified by query 
+ exports.elephant_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Elephant.findById( req.query.id) 
+        res.render('elephantdetail',  
+{ title: 'Elephant Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+   // Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.elephant_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('elephantcreate', { title: 'Elephant Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
+// Handle building the view for updating a costume.
+// query provides the id
+exports.elephant_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Elephant.findById(req.query.id)
+    res.render('elephantupdate', { title: 'Elephant Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/elephant.js
+++ routes/elephant.js
@@ -13,3 +13,12 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', elephant_controlers.elephant_view_all_Page );
 module.exports = router;
+
+/* GET detail costume page */ 
+router.get('/detail', elephant_controlers.elephant_view_one_Page); 
+
+/* GET create costume page */
+router.get('/create', elephant_controlers.elephant_create_Page);
+
+/* GET create update page */
+router.get('/update', elephant_controlers.elephant_update_Page);
